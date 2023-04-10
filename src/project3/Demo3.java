2dee6fe03c07c5fe6db0d07ea57b08015b13bbc2
hamzasadeq <hamzasadeq@10.0.0.7>
2023-04-10 14:32:07 -0400

    Stash4

---
+++ src/project3/Demo3.java
@@ -0,0 +1,22 @@
+package project3;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+	
+	public static void getName() {
+		
+		System.out.println("Nooteck");
+		
+		
+	}
+	
+	
+}
